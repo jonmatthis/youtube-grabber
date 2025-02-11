281ef3f425b1f69bc558390ff0809db57af86bd8
jonmatthis <jonmatthis@gmail.com>
2025-02-11 01:56:10 +0000

    Feb 10, 2025, 8:56 PM

--- youtube_downloader.js
+++ youtube_downloader.js
@@ -1,6 +1,5 @@
-import * as fs from 'fs';
-import ytdl from 'ytdl-core';
-
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
   .pipe(fs.createWriteStream('video.mp4'));
